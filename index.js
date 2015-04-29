84e44f07f5f64e387ea41c123ef4d6a35690b346
zmofei <13761509829@163.com>
2015-04-29 18:36:29 +0800

    update index

--- index.js
+++ index.js
@@ -29,25 +29,27 @@ function requireRoute(router) {
     return require('./route/' + router + '.js');
 }
 
-//
+
+//index
 app.use(route.get('/', requireRoute('index')));
 
-//
+//blog
 app.use(route.get('/blog/:class/:page', requireRoute('blog')));
 app.use(route.get('/blog/:class', requireRoute('blog')));
 app.use(route.get('/blog', requireRoute('blog')));
-//
 app.use(route.post('/blog/articleComment', requireRoute('blog_comment')));
 
-//
+//verification
 app.use(route.get('/verification/img', requireRoute('/verification/img')));
 
-//
+//about
 app.use(route.get('/about', function*() {
     this.redirect('/#about')
 }));
 
-//
+// app.use()
+
+//lab
 app.use(route.get('/lab', requireRoute('lab')));
 
 //listen the port
