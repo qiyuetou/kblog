fdb74755944b075c269e6c2fd313ccb6c4d8068a
zmofei <13761509829@163.com>
2015-05-05 23:31:51 +0800

    update

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ function requireRoute(router) {
 app.use(router(app));
 
 //index
-// app.use(route.get('/', requireRoute('index')));
 app.get('/', requireRoute('index'));
 
 //blog
@@ -50,31 +49,27 @@ app.get('/blog', requireRoute('blog'));
 app.get('/blog/:Tclass/:Tpage', requireRoute('blog'));
 app.get('/blog/:Tclass', requireRoute('blog'));
 app.post('/blog/articleComment', requireRoute('blog_comment'));
-// app.use(route.get('/blog', requireRoute('blog')));
-// app.use(route.get('/blog/:class/:page', requireRoute('blog')));
-// app.use(route.get('/blog/:class', requireRoute('blog')));
-// app.use(route.post('/blog/articleComment', requireRoute('blog_comment')));
 
 //verification
-app.use(route.get('/verification/img', requireRoute('/verification/img')));
+app.get('/verification/img', requireRoute('/verification/img'));
 
 //about
-app.use(route.get('/about', function*() {
+app.get('/about', function*() {
     this.status = 301;
     this.redirect('/#about');
-}));
+});
 
 //rss
-app.use(route.get('/rss', requireRoute('/rss')));
+app.get('/rss', requireRoute('/rss'))
 
 //api
-app.use(route.post('/api/sendemail', requireRoute('/api/sendemail')));
+app.post('/api/sendemail', requireRoute('/api/sendemail'));
 
 //lab
-app.use(route.get('/lab', requireRoute('lab')));
+app.get('/lab', requireRoute('lab'));
 
 //lab
-app.use(route.get('/links', requireRoute('links')));
+app.get('/links', requireRoute('links'));
 
 //listen the port
 app.listen(5123);
